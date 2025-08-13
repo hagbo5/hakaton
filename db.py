1dfc600dddeb90b06ab2df14b1cad7a507b26abc
Hugo Guerrero <123345798+hagbo5@users.noreply.github.com>
2025-08-12 23:25:29 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
+import os
 import mysql.connector
 
 connection = mysql.connector.connect(
-    host="sql5.freesqldatabase.com",
-    user="sql5794797",
-    password="eqtdK9Trrg",
-    database="sql5794797",
-    port=3306
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    port=int(os.getenv("DB_PORT", 3306))  # 3306 como valor por defecto
 )
