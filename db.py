b6bfacea694f2e46d06937133478ead64bb2c86d
Hugo Guerrero <123345798+hagbo5@users.noreply.github.com>
2025-08-12 23:54:53 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
 import os
 import mysql.connector
 
-connection = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    port=int(os.getenv("DB_PORT", 3306))  # 3306 como valor por defecto
-)
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))  # 3306 como valor por defecto
+    )
+    return connection
